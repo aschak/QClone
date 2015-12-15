43cca8322702af28c512c755319ce77c216467fa
Kareem Ascha <aschak2@gmail.com>
2015-12-15 11:15:48 -0800

    update question controller

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,36 +1,51 @@
 class QuestionsController < ApplicationController
 
   def index
-
+    @questions = Question.all
   end
 
   def show
-
+    @question = Question.find(params[:id])
   end
 
   def new
-
+    @question = Question.new
   end
 
   def create
+    @question = current_user.questions.new(question_params)
+
+    if @question.save
+      render json: @question
+    else
+      render json: @question.errors.full_messages, status: "Unprocessable Entity"
+    end
 
   end
 
   def edit
-
+    @question = Question.find(params[:id])
   end
 
   def update
+    @question = Question.find(params[:id])
 
+    if @question.update(question_params)
+      render json: @question
+    else
+      render json: @question.errors.full_messages, status: "Unprocessable Entity"
+    end
   end
 
   def destroy
-
+    question = Question.find(params[:id])
+    question.destroy
+    redirect_to root_url
   end
 
   private
 
   def question_params
-
+    params.require(:question).permit(:title, :body)
   end
 end
