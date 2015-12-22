129e4cd77d380609d40ad3f64afa99a5669807c2
Kareem Ascha <aschak2@gmail.com>
2015-12-21 20:47:29 -0800

    refactor tag flux cycle

---
+++ frontend/components/tags/tagIndex.jsx
@@ -0,0 +1,20 @@
+var React = require('react'),
+    TagStore = require('../../stores/tag.js'),
+    TagActions = require('../../actions/tag_actions.js'),
+    TagIndexItem = require('./tagIndexItem.jsx');
+
+
+var TagIndex = React.createClass({
+
+
+  render: function () {
+    return(
+      <div>
+
+      </div>
+    );
+  }
+});
+
+
+module.exports = TagIndex;
---
+++ frontend/components/tags/tagIndexItem.jsx
@@ -0,0 +1,20 @@
+var React = require('react'),
+    TagActions = require('../../actions/tag_actions'),
+    History = require('react-router').History;
+
+
+
+var TagIndexItem = React.createClass({
+
+
+  render: function () {
+    return (
+      <div>
+
+      </div>
+    );
+  }
+});
+
+
+module.exports = TagIndexItem;
