01c897a97afe4eaa05d5ef4ba97b7d96daaab8f9
Kareem Ascha <aschak2@gmail.com>
2015-12-21 17:04:01 -0800

    add taggings model, stylize nav bar

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,2 +1,11 @@
 class Tag < ActiveRecord::Base
+  validates :tag_name, presence: true, uniqueness: true
+
+  has_many :taggings,
+    primary_key: :id,
+    foreign_key: :tag_id,
+    class_name: "Tagging"
+
+  has_many :questions,
+    through: :taggings 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151221233002) do
+ActiveRecord::Schema.define(version: 20151221235140) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -48,6 +48,16 @@ ActiveRecord::Schema.define(version: 20151221233002) do
 
   add_index "questions", ["author_id"], name: "index_questions_on_author_id", using: :btree
   add_index "questions", ["title"], name: "index_questions_on_title", unique: true, using: :btree
+
+  create_table "taggings", force: :cascade do |t|
+    t.integer  "tag_id",      null: false
+    t.integer  "question_id", null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  add_index "taggings", ["question_id"], name: "index_taggings_on_question_id", using: :btree
+  add_index "taggings", ["tag_id"], name: "index_taggings_on_tag_id", using: :btree
 
   create_table "tags", force: :cascade do |t|
     t.string   "tag_name",   null: false
